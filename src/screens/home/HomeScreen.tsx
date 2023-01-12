db987bcb3d0056463799e9b58a360ad5e35829e2
Cesar Henrique <cesar_silk321@hotmail.com>
2023-01-12 17:33:49 -0300

    ajustes visuais e ajuste no linter para permitir style inline

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -16,14 +16,17 @@ const CardHeader = (props: any) => (
     <View style={[styles.cardHeaderItems, {maxWidth: 40}]}>
       <Icon style={styles.icon} fill="#36f" name="globe-2-outline" />
     </View>
-    <View style={[styles.cardHeaderItems, {flex: 3}]}>
-      <Text category="h6">Produção de hoje</Text>
+    <View style={[styles.cardHeaderItems, {flex: 2}]}>
+      <Text style={{fontSize: 22}} category="h6">
+        Produção de hoje
+      </Text>
     </View>
-    <View style={[styles.cardHeaderItems]}>
+    <View style={styles.cardHeaderItems}>
       <Text
         status="success"
         style={{
           textAlign: 'right',
+          fontSize: 16,
         }}
         category="s1">
         Sex. 06 de Janeiro
@@ -73,40 +76,64 @@ class HomeScreen extends React.Component<any, any> {
               header={CardHeader}
               // footer={CardFooter}
               style={styles.card}>
-              <Text style={{marginBottom: 8}} category="h6">
+              <Text style={styles.title} category="h6">
                 Banco de horas:
               </Text>
               <View style={styles.flexRow}>
                 <View style={styles.flex}>
-                  <Text category="label">Aprovadas:</Text>
-                  <Text category="s1">+35:54h</Text>
+                  <Text style={styles.label} category="label">
+                    Aprovadas:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    +35:54h
+                  </Text>
                 </View>
                 <View style={styles.flex}>
-                  <Text category="label">Em Analise:</Text>
-                  <Text category="s1">+35:54h</Text>
+                  <Text style={styles.label} category="label">
+                    Em Analise:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    +35:54h
+                  </Text>
                 </View>
               </View>
               <View style={styles.flexRow}>
                 <View style={styles.flex}>
-                  <Text category="label">Jornada:</Text>
-                  <Text category="s1">8h</Text>
+                  <Text style={styles.label} category="label">
+                    Jornada:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    8h
+                  </Text>
                 </View>
                 <View style={styles.flex}>
-                  <Text category="label">Carga horária:</Text>
-                  <Text category="s1">176h</Text>
+                  <Text style={styles.label} category="label">
+                    Carga horária:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    176h
+                  </Text>
                 </View>
               </View>
               <View style={styles.flexRow}>
                 <View style={styles.flex}>
-                  <Text category="label">Dias úteis:</Text>
-                  <Text category="s1">22</Text>
+                  <Text style={styles.label} category="label">
+                    Dias úteis:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    22
+                  </Text>
                 </View>
                 <View style={styles.flex}>
-                  <Text category="label">Restante:</Text>
-                  <Text category="s1">151h</Text>
+                  <Text style={styles.label} category="label">
+                    Restante:
+                  </Text>
+                  <Text style={styles.text} category="s1">
+                    151h
+                  </Text>
                 </View>
               </View>
-              <Text style={{marginVertical: 8}} category="h6">
+              <Text style={styles.title} category="h6">
                 Entradas/Saídas:
               </Text>
               {data.length > 0 ? (
@@ -124,7 +151,7 @@ class HomeScreen extends React.Component<any, any> {
                       },
                     ]}>
                     <Icon
-                      style={[styles.icon, {width: 24, height: 24}]}
+                      style={[styles.icon, {width: 34, height: 34}]}
                       fill={item.tipo === 'entrada' ? 'green' : 'red'}
                       name={
                         item.tipo === 'entrada'
@@ -132,7 +159,7 @@ class HomeScreen extends React.Component<any, any> {
                           : 'log-out-outline'
                       }
                     />
-                    <Text style={{marginLeft: 8, fontSize: 16}} category="s1">
+                    <Text style={{marginLeft: 8, fontSize: 22}} category="s1">
                       {format(item.hora, 'HH:mm:ss')}
                     </Text>
                   </View>
@@ -200,6 +227,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
     height: 32,
+  },
+  title: {
+    marginBottom: 8,
+    fontSize: 24,
+  },
+  label: {
+    fontSize: 18,
+  },
+  text: {
+    fontSize: 22,
   },
   cardHeaderItemText: {},
   flexRow: {
